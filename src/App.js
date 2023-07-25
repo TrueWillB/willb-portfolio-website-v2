df3c4961d35629fbd0e82acf140c1706cda4574a
William Brown <willis291@gmail.com>
2023-07-24 23:32:50 -0600

    first push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import MainSite from "./components/MainSite";
+
+function App() {
+  return (
+    <div className="portfolio-app">
+      <h1>Portfolio</h1>
+      <MainSite />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componenets/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="portfolio-app">
+      <header>
+        <h1>This is text from the Header component</h1>
+      </header>
+    </div>
+  );
+}
